82b97e5cb87d464134902680dcdd1d7dea4b7c17
Edgar Lule <edgar21lc@gmail.com>
2021-08-02 23:16:28 -0700

    added a workout model and html routes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,13 +10,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
